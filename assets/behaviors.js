072814fefa85bc971e47ede3b5608489706f2417
Andrew Hedges <andrew@hedges.name>
2019-07-29 09:13:35 -0700

    Stubbed in a photo carousel for the homepage and a code of conduct page

---
+++ assets/behaviors.js
@@ -0,0 +1,8 @@
+/*
+
+Carousel behavior:
+
+1. Detect that all photos have loaded, then add 'ready' class to all photos that don't already have it
+2. Every 1 second, cross-fade to a new photo (i.e. add 'selected' class to the next photo and remove it from the current photo)
+
+*/
