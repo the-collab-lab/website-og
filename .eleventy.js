d886f1fa9cdcc649547fbbb245e42d2df2f6bc63
Alejandro Nanez <alejonanez@gmail.com>
2020-09-19 17:07:15 -0500

    Update the output for GA verification tag. Copy favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("pages/assets");
+  eleventyConfig.addPassthroughCopy("pages/favicon.ico");
+  eleventyConfig.addPassthroughCopy({
+    "pages/google657107b6feadb517.html": "google657107b6feadb517.html",
+  });
   return {
     dir: {
       input: "pages",
