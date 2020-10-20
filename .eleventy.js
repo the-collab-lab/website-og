922cc36aa93726e4b115871d4b9bdd721e00fada
Alejandro Nanez <alejonanez@gmail.com>
2020-10-19 19:20:31 -0500

    Installed Prettier and Husky
    - Run prettier on all the files
    - Prettier will on every staged file in git

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("pages/assets");
-  eleventyConfig.addPassthroughCopy("pages/favicon.ico");
+  eleventyConfig.addPassthroughCopy('pages/assets');
+  eleventyConfig.addPassthroughCopy('pages/favicon.ico');
   eleventyConfig.addPassthroughCopy({
-    "pages/google657107b6feadb517.html": "google657107b6feadb517.html",
+    'pages/google657107b6feadb517.html': 'google657107b6feadb517.html',
   });
   return {
     dir: {
-      input: "pages",
-      output: "build",
+      input: 'pages',
+      output: 'build',
     },
   };
 };
