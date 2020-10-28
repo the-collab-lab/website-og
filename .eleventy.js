7059008832e318d93752fd1b6b4f7b969be4b403
Alejandro Nanez <alejonanez@gmail.com>
2020-10-27 20:11:56 -0500

    Removed filter  in 11ty config - we don't need that.

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     'pages/google657107b6feadb517.html': 'google657107b6feadb517.html',
   });
-  eleventyConfig.addFilter('emptyString', function (value) {
-    return !!value.length;
-  });
   return {
     dir: {
       input: 'pages',
