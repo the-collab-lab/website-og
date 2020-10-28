72beb3c69e2e94113f255d11c06833746ffa68f2
Andrew Hedges <andrew.hedges@zapier.com>
2020-10-27 19:01:25 -0700

    Change calculatedDate to show the year only once if the start and end are in the same year

--- pages/graphql/data.js
+++ pages/graphql/data.js
@@ -9,11 +9,22 @@ const { TeamsQuery } = require('./queries');
  *
  * Used in the Teams section.
  */
-const calculatedDate = ({ startDate, endDate }) =>
-  `${format(parseISO(startDate), 'MMMM-y')} - ${format(
-    parseISO(endDate),
-    'MMMM-y',
-  )}`;
+const calculatedDate = ({ startDate, endDate }) => {
+  let startMask = 'MMMM y';
+  let endMask = 'MMMM y';
+
+  // if the years are the same, don’t show the year twice
+  // e.g., "December 2019 – January 2020"
+  // e.g., "October – November 2020"
+  if (startDate.slice(0, 4) === endDate.slice(0, 4)) {
+    startMask = 'MMMM';
+  }
+
+  let formattedStartDate = format(parseISO(startDate), startMask);
+  let formattedEndDate = format(parseISO(endDate), endMask);
+
+  return `${formattedStartDate} – ${formattedEndDate}`;
+};
 
 const graphQLEndpoint =
   'https://api-us-east-1.graphcms.com/v2/ckfwosu634r7l01xpco7z3hvq/master';
