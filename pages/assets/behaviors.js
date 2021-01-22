ca4c4178119d18d67af513a8272effc6ae583cfa
Andrew Hedges <andrew@hedges.name>
2021-01-21 19:57:41 -0800

    Factor out inline script

--- pages/assets/behaviors.js
+++ pages/assets/behaviors.js
@@ -1,9 +1,25 @@
 import { Gallery } from './gallery.js';
+import { DonationForm } from './donation-form.js';
 import { DonationNotification } from './donation-notification.js';
+
+function initDonationForm() {
+  if (null !== document.getElementById('donation')) {
+    // initialize Stripe using your publishable key
+    const stripe = Stripe('{{ stripePublishableKey }}');
+
+    // find the buttons and error message elements
+    const checkoutButtons = document.getElementsByClassName(
+      'checkout-button',
+    );
+
+    new DonationForm({ stripe, checkoutButtons });
+  }
+}
 
 function init() {
   new Gallery();
   new DonationNotification();
+  initDonationForm();
 }
 
 document.addEventListener('DOMContentLoaded', init);
