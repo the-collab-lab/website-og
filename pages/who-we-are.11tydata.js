2e0e88567f8dd555e36b42ffa7718c778684abb6
Isa Abutaa <isaabutaa@gmail.com>
2022-03-19 11:12:28 -0400

    184 - remove mentors from who-we-are.11tydata.js

--- pages/who-we-are.11tydata.js
+++ pages/who-we-are.11tydata.js
@@ -1,14 +1,12 @@
-const { getTeams, getMentors, getAdvisors, getFounders } = require('./graphql/data');
+const { getTeams, getAdvisors, getFounders } = require('./graphql/data');
 
 module.exports = async () => {
   const teams = await getTeams();
-  const mentors = await getMentors();
   const advisors = await getAdvisors();
   const founders = await getFounders();
 
   return {
     teams,
-    mentors,
     advisors,
     founders,
   };
