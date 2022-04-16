4eb38e27a90c3e4580803ebb4875ac7153d624c8
stacie.taylor <stacie.taylor@zapier.com>
2022-04-16 09:55:40 -0700

    chore: Add data from mentor page to volunteer page

--- pages/volunteer.11tydata.js
+++ pages/volunteer.11tydata.js
@@ -1,6 +1,9 @@
-const { getVolunteers } = require('./graphql/data');
+const { getPages, getVolunteers } = require('./graphql/data');
 
 module.exports = async () => {
   const volunteers = await getVolunteers();
-  return { volunteers };
+  const pages = await getPages();
+  const filtered = pages.filter(page => '/mentor/' === page.slug);
+  let html = filtered.length > 0 ? filtered[0].html : '';
+  return { html, volunteers };
 };
