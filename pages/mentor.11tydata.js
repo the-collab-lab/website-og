dbda383ac44ef3982532e7e1b5e00caa8ee2aa73
stacie.taylor <stacie.taylor@zapier.com>
2022-04-15 19:11:30 -0700

    chore: Remove mentor from navigation

--- pages/mentor.11tydata.js
+++
@@ -1,8 +0,0 @@
-const { getPages } = require('./graphql/data');
-
-module.exports = async () => {
-  const pages = await getPages();
-  const filtered = pages.filter(page => '/mentor/' === page.slug);
-  let html = filtered.length > 0 ? filtered[0].html : '';
-  return { html };
-};
