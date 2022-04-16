62aeb183926832438babb5aa9d2d349a51c5dbbb
stacie.taylor <stacie.taylor@zapier.com>
2022-04-16 09:49:31 -0700

    chore: Add apply page data to participate page

--- pages/participate.11tydata.js
+++ pages/participate.11tydata.js
@@ -1,11 +1,16 @@
-const { getTeams, getAdvisors, getFounders } = require('./graphql/data');
+const { getPages, getTeams, getAdvisors, getFounders } = require('./graphql/data');
 
 module.exports = async () => {
   const teams = await getTeams();
   const advisors = await getAdvisors();
   const founders = await getFounders();
 
+  const pages = await getPages();
+  const filtered = pages.filter(page => '/apply/' === page.slug);
+  let html = filtered.length > 0 ? filtered[0].html : '';
+
   return {
+    html,
     teams,
     advisors,
     founders,
