88d18b0a40180cb7a5e06c2a5c908c4744807ae8
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-07-27 12:21:35 -0700

    chore: use context-based env vars

--- pages/_data/config.js
+++ pages/_data/config.js
@@ -1,11 +1,9 @@
-const isProduction = () => process.env.CONTEXT === 'production';
+// Netlify will change the values of these keys depending on
+// the context in whoch the deploy is run, so we don't need to check
+// what environment we're in.
+// @see: https://docs.netlify.com/netlify-labs/experimental-features/environment-variables/
 
 module.exports = {
-  STRIPE_SECRET_KEY: isProduction()
-    ? process.env.STRIPE_SK_LIVE
-    : process.env.STRIPE_SK_TEST,
-
-  STRIPE_PUBLISHABLE_KEY: isProduction()
-    ? process.env.STRIPE_PK_LIVE
-    : process.env.STRIPE_PK_TEST,
+  STRIPE_SECRET_KEY: process.env.STRIPE_SK,
+  STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PK,
 };
