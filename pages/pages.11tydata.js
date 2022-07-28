420285d0dc014aa21691a5eed1ae9ad91c2fa600
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-07-28 09:06:55 -0700

    feat: change namespace of pricing options

--- pages/pages.11tydata.js
+++ pages/pages.11tydata.js
@@ -3,47 +3,45 @@ const {
   getTestimonials,
 } = require('./graphql/data');
 const Stripe = require('stripe');
-const config = require('./_data/config');
+const { STRIPE_PUBLISHABLE_KEY, STRIPE_SECRET_KEY } = require('./_data/config');
 
 module.exports = async () => {
   const block = await getFrontPageApplicationBlock();
   const applicationBanner = `<div class="banner">${block.textBlocks[0].textContent.html}</div>`;
 
-  const getDonationOptions = async () => {
+  const getDonationConfig = async () => {
     try {
-      const stripe = Stripe(config.STRIPE_SECRET_KEY);
+      const stripe = Stripe(STRIPE_SECRET_KEY);
       const { data: products = [] } = await stripe.products.list();
 
-      const options = await Promise.all(
+      const _options = await Promise.all(
         products.map(async (option) => {
           const { data: prices } = await stripe.prices.list({
             product: option.id,
           });
-          const price = {
+
+          option.price = {
             ...prices[0],
             formatted_amount: prices[0].unit_amount / 100,
           };
 
-          return { ...option, price };
+          return option;
         }),
       );
 
-      const donationOptions = (options || []).sort(
+      const options = _options?.sort(
         (a, b) => parseInt(a.price.unit_amount) - parseInt(b.price.unit_amount),
       );
 
-      const stripePublishableKey = config.STRIPE_PUBLISHABLE_KEY;
       return {
-        isDonationEnabled:
-          donationOptions.length > 0 && stripePublishableKey !== undefined,
-        donationOptions,
-        stripePublishableKey,
+        isEnabled: options?.length > 0 && STRIPE_PUBLISHABLE_KEY !== undefined,
+        options,
       };
     } catch (err) {
       /** The view is prepared to handle empty donation options, so we can just return an empty list here */
       return {
-        isDonationEnabled: false,
-        donationOptions: [],
+        isEnabled: false,
+        options: [],
       };
     }
   };
@@ -52,6 +50,6 @@ module.exports = async () => {
     layout: 'mlayout.liquid',
     applicationBanner,
     testimonials: await getTestimonials(),
-    donationOptions: await getDonationOptions(),
+    donationConfig: await getDonationConfig(),
   };
 };
