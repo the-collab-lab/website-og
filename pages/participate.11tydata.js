9497e96b250bbf491cf18a5632a4d79eadc3f212
Stacie Taylor <stacietaylorcima@gmail.com>
2022-12-06 12:25:05 -0800

    Change 11tydata to filter for participate page rather than apply page
    The application questions are no longer appearing on the website. It appears to be a problem with the 11tydata filtering for an old page name. This commit updates that data to filter on `participate` rather than `apply`

--- pages/participate.11tydata.js
+++ pages/participate.11tydata.js
@@ -2,7 +2,7 @@ const { getPages } = require('./graphql/data');
 
 module.exports = async () => {
   const pages = await getPages();
-  const filtered = pages.filter((page) => '/apply/' === page.slug);
+  const filtered = pages.filter((page) => '/participate/' === page.slug);
   let html = filtered.length > 0 ? filtered[0].html : '';
 
   return {
